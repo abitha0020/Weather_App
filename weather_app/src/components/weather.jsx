5961f69b975998b8fd3bf47aa6f544bbf8844998
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-17 15:40:20 +0530

    Update weather.jsx

--- weather_app/src/components/weather.jsx
+++ weather_app/src/components/weather.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Weather = ({ weatherData }) => {
-  return (
-    <div>
-      <h1>Weather Data</h1>
-      <p>{weatherData.weather[0].description}</p>
-    </div>
-  );
+    
+    return (
+        <div>
+        <h1>Weather Data</h1>
+        <p>{weatherData.weather[0].description}</p>
+        </div>
+    );
 };
 
 export default Weather;

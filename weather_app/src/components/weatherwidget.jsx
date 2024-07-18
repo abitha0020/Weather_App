786a101396758307ce12646ebe58376e26b679e8
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-18 13:43:26 +0530

    Update weatherwidget.jsx

--- weather_app/src/components/weatherwidget.jsx
+++ weather_app/src/components/weatherwidget.jsx
@@ -1,8 +1,8 @@
 export default function WeatherWidget(props) {
     return(
-        <div className="border border-black h-[200px] w-[250px]">
-            <h2>{props.title}</h2>
-            <p>{props.value}</p>
+        <div className="border border-black h-[100px] w-[250px]">
+            <h2 className="text-center p-3">{props.title}</h2>
+            <p className="text-center">{props.value}</p>
         </div>
     );
 } 

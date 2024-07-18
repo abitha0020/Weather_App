9198affe29c3648ef6d30f2b8355a1c1be533599
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-18 15:39:08 +0530

    Update locationInput.jsx

--- weather_app/src/components/locationInput.jsx
+++ weather_app/src/components/locationInput.jsx
@@ -11,7 +11,7 @@ export default function LocationInput({onLocationChange}) {
     return (
         <div className="w-[1000px] flex justify-between">
             <div>
-                <label htmlFor="location">Enter Location To Search: </label>
+                <label htmlFor="location" className="font-medium text-base">Enter Location To Search: </label>
                 <input type="text"id="location" value={location} onChange={handleChange}/>
             </div>
             <button onClick={handleclick} className="w-[140px] h-[30px] leading-6 text-[17px] tracking-[0.2px] border border-black"> search</button>
